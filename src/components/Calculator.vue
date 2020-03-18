714158b2d16a2405eec6a4434f13dab4f0f6bc64
Abhiram Pai <abhirampai1999@gmail.com>
2020-03-18 09:23:24 +0530

    Add files via upload

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -2,24 +2,24 @@
   <div class="calculator">
     <div class="display">{{current || '0'}}</div>
     <div @click="clear" class="btn ac">AC</div>
-     <div class="btn ac">+/-</div>
-      <div class="btn ac">%</div>
-       <div class="btn op">/</div>
-        <div class="btn">7</div>
-         <div class="btn">8</div>
-          <div class="btn">9</div>
-           <div class="btn op">x</div>
-            <div class="btn">4</div>
-             <div class="btn">5</div>
-              <div class="btn">6</div>
-               <div class="btn op">-</div>
-                <div class="btn">1</div>
-                 <div class="btn">2</div>
-                  <div class="btn">3</div>
-                  <div class="btn op">+</div>
-                  <div class="btn zero">0</div>
-                  <div class="btn">.</div>
-                  <div class="btn op">=</div>
+     <div @click="sign" class="btn ac">+/-</div>
+      <div @click="percentage" class="btn ac">%</div>
+       <div @click="divide" class="btn op">/</div>
+        <div @click="append(7)" class="btn">7</div>
+         <div @click="append(8)" class="btn">8</div>
+          <div @click="append(9)" class="btn">9</div>
+           <div  @click="multi" class="btn op">x</div>
+            <div @click="append(4)" class="btn">4</div>
+             <div @click="append(5)" class="btn">5</div>
+              <div @click="append(6)" class="btn">6</div>
+               <div @click="sub" class="btn op">-</div>
+                <div @click="append(1)" class="btn">1</div>
+                 <div @click="append(2)" class="btn">2</div>
+                  <div @click="append(3)" class="btn">3</div>
+                  <div @click="add" class="btn op">+</div>
+                  <div @click="append(0)" class="btn zero">0</div>
+                  <div @click="dot" class="btn">.</div>
+                  <div @click="equal" class="btn op">=</div>
   </div>
 </template>
 
@@ -28,13 +28,58 @@ export default {
   name: 'Calculator',
   data(){
     return{
-      current: '123',
+      previous:null,
+      current: '',
+      operator: null,
+      operatorclicked:false,
     }
 
   },
   methods:{
     clear(){
       this.current="";
+    },
+    sign(){
+      this.current=this.current.charAt(0)==='-'?this.current.slice(1):`-${this.current}`
+    },
+    percentage(){
+      this.current=`${parseFloat(this.current)/100}`
+    },
+    append(number){
+      if(this.operatorclicked){
+        this.current='';
+        this.operatorclicked = false;
+      }
+      this.current=`${this.current}${number}`
+    },
+    dot(){
+      if(this.current.indexOf('.')===-1){
+        this.append('.')
+      }
+    },
+    setPrevious(){
+      this.previous = this.current;
+      this.operatorclicked=true;
+    },
+    divide(){
+      this.operator = (a,b) => a/b;
+      this.setPrevious();
+    },
+    multi(){
+      this.operator = (a,b) => a*b;
+      this.setPrevious();
+    },
+    sub(){
+      this.operator = (a,b) => a-b;
+      this.setPrevious();
+    },
+    add(){
+      this.operator = (a,b) => a+b;
+      this.setPrevious(); 
+    },
+    equal(){
+      this.current=`${this.operator(parseFloat(this.current),parseFloat(this.previous))}`;
+      this.previous = null;
     }
   }
 }
